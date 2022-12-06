5b57747b8ec8dbb17b209e9599e7bd17dea6c0d2
Aurélien Charpilienne <aurelien.charpilienne@gmail.com>
2022-12-06 20:18:16 +0100

    feat: wave box design update

--- src/components/wave-box/wave-box.tsx
+++ src/components/wave-box/wave-box.tsx
@@ -4,27 +4,31 @@ import "./wave-box.css";
 
 const WaveBox = () => {
   const { t } = useTranslation();
+  const totalWaves = 12;
 
   return (
     <div className="flex justify-center">
       <div className="wave-box flex">
         <div style={{ flex: 2.5 }}>
-          <div style={{ marginBottom: 10 }}>
+          <div className="margin-bottom-10">
             {t("waveHeadline1")}
           </div>
-          <div>
+          <div className="margin-bottom-10">
             {t("waveHeadline2")}
           </div>
           <div>
             {t("waveHeadline3")}
           </div>
         </div>
-        <div style={{ flex: 1 }}>
+        <div style={{ flex: 1, marginLeft: 25 }}>
           <div>
             {t("yourTotalWaves")}
+            <span className="font-bold">
+              {totalWaves}
+            </span>
           </div>
-          <div>
-            {t("waveAtMe")}
+          <div className="wave-at-me-button">
+            {t("waveAtMe")} 👋
           </div>
         </div>
       </div>
