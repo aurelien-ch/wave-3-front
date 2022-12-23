c2d1866dee5dca932b98bf9098e0961fc47a8ddc
Aurélien Charpilienne <aurelien.charpilienne@gmail.com>
2022-12-23 23:42:00 +0200

    refactor: env updated

--- public/env.js
+++ public/env.js
@@ -1,5 +1,5 @@
 window.env = {
-  "CONTRACT_ADDRESS": "0x79f6fc5Dec31817cE2f8604cFC258E1357724F20",
+  "CONTRACT_ADDRESS": "0xC3B2Cf20fc08C187344823bc1509b071482Ebf84",
   "CHAIN_ID": "5",
   "GITHUB_LINK": "https://github.com/aurelien-ch/",
   "LINKEDIN_LINK": "https://www.linkedin.com/in/aurelien-charpilienne/",
