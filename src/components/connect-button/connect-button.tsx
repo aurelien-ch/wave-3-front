6b5e2acd498b3161e98d9639231cbe4743d2279c
Aurélien Charpilienne <aurelien.charpilienne@gmail.com>
2022-12-24 23:53:25 +0200

    feat: connect metamask from mobile

--- src/components/connect-button/connect-button.tsx
+++ src/components/connect-button/connect-button.tsx
@@ -1,3 +1,4 @@
+import { useMediaQuery } from "react-responsive";
 import { useTranslation } from "react-i18next";
 
 import { useStore } from "../../providers/state/state-provider";
@@ -6,14 +7,23 @@ import { useMetamaskProvider } from "../../providers/metamask-provider";
 import "./connect-button.css";
 
 const ConnectButton = () => {
+  const isMobile = useMediaQuery({ query: "(max-width: 700px)" });
   const { t } = useTranslation();
   const metamaskProvider = useMetamaskProvider();
   const metamaskAccount = useStore(state => state.metamaskAccount);
 
+  const connectAccount = () => {
+    if (isMobile) {
+      window.open("https://metamask.app.link/dapp/" + window.location.href, "_blank");
+    } else {
+      metamaskProvider.connectAccount();
+    }
+  };
+
   return (
     <div
       className={`connect-button ${metamaskAccount ? "connected" : ""}`}
-      onClick={() => metamaskProvider.connectAccount()}
+      onClick={connectAccount}
     >
       {metamaskAccount ? metamaskProvider.formatAddress(metamaskAccount) : t("header.connect")}
       {
